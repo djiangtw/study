560894c34ddbcd1abf1893adcc3335bd0b7da9f6
Danny Jiang <xinyi904314@yahoo.com.tw>
2015-09-12 15:32:46 +0000

    exchange files

--- c/strassen/strassen.c
+++ c/strassen/strassen.c
@@ -1,92 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*#include "matrix.h"*/
-#include "strassen.h"
+#include "s.h"
 
-extern matrix_s new_matrix_s(double** a);
-/*
- * strassen.c
- *
- * Courtesy [Buhler 1993].
- * Routines to realize the Strassen recursive matrix multiplication.
- * Multiply n by n matrices a and b, putting the result in c, and
- * using the matrix d as scratch space.  The Strassen algorithm is:
- *
- *      q7 = (a12-a22)(b21+b22)
- *      q6 = (-a11+a21)(b11+b12)
- *      q5 = (a11+a12)b22
- *      q4 = a22(-b11+b21)
- *      q3 = a11(b12-b22)
- *      q2 = (a21+a22)b11
- *      q1 = (a11+a22)(b11+b22)
- *      c11 = q1+q4-q5+q7
- *      c12 = q3+q5
- *      c21 = q2+q4
- *      c22 = q1+q3-q2+q6
- *
- * where the double indices refer to submatrices in an obvious way.
- * Each line of multiply() that recursively calls itself computes one
- * of the q's.  Four scratch half-size matrices are required by the
- * sequence of computations here; with some rearrangement this
- * storage requirement can be reduced to three half-size matrices.
- *
- * The small matrix computations (i.e., for n <= BREAK) can be
- * optimized considerably from those given here; in particular, this
- * is important to do before the value of BREAK is chosen optimally.
- *
- */
+int g_break = 4;
+//extern void add(int n, matrix_s a, matrix_s b, matrix_s c);
+#define a11 p[0]
+#define a12 p[1]
+#define a21 p[2]
+#define a22 p[3]
+#define b11 q[0]
+#define b12 q[1]
+#define b21 q[2]
+#define b22 q[3]
+#define c11 r[0]
+#define c12 r[1]
+#define c21 r[2]
+#define c22 r[3]
+#define d11 s[0]
+#define d12 s[1]
+#define d21 s[2]
+#define d22 s[3]
 
-void update_matrix_s(matrix_s a, int n)
-{
-    /*printf("%x\n", (unsigned int)a);*/
-    /*printf("%x\n", (unsigned int)a->p[0]);*/
-    /*printf("%x\n", (unsigned int)a->p[1]);*/
-
-    /*a->p[0]->d = a->d;*/
-    /*a->p[1]->d = &a->d[0] + n / 2;*/
-    /*a->p[2] = a->p[2][n / 2];*/
-    /*a->p[3] = a->p[3]d[n / 2] + n / 2;*/
-
-    /*printf("%x\n", (unsigned int)a);*/
-    /*printf("%x\n", (unsigned int)a->p[0]);*/
-    /*printf("%x\n", (unsigned int)a->p[1]);*/
-}
-
-void recover_matrix_s(matrix_s a)
-{
-    a->p[0] = a->p[4];
-}
-
-double*** split_matrix_s(matrix_s a, int n)
+double*** arr_qsplit(double** a, int n)
 {
     int i;
-    double*** p;
+    double*** r;
 
-    p = (double***)malloc(sizeof(double**)*4);
+    r = (double***)malloc(sizeof(double**) * 4);
     for(i = 0; i < 4; i++)
     {
-        p[i] = malloc(sizeof(double*)*n);
+        r[i] = (double**)malloc(sizeof(double*) * n);
     }
-    for(i=0; i < n; i++)
+    for(i = 0; i < n; i++)
     {
-        p[0] = &a->d[i];
-        p[1] = &a->d[i] + n;
-        p[2] = &a->d[i + n];
-        p[3] = &a->d[i + n] + n;
+        r[0][i] = a[i];
+        r[1][i] = a[i] + n;
+        r[2][i] = a[i + n];
+        r[3][i] = a[i + n] + n;
     }
-
-    return p;
+    return r;
 }
-/* c = a*b */
-void multiply(int n, matrix_s a, matrix_s b, matrix_s c, matrix_s d)
-{
-    update_matrix_s(a, n);
-    update_matrix_s(b, n);
-    update_matrix_s(c, n);
-    update_matrix_s(d, n);
 
-    if (n <= BREAK) {
-        double sum, **p = a->d, **q = b->d, **r = c->d;
+void arr_qsplit_recall(double*** a)
+{
+    int i;
+    for(i = 0; i < 4; i++)
+    {
+        free(a[i]);
+    }
+    free(a);
+}
+
+void s_mul(int n, double** a, double** b, double** c, double** d)
+{
+    if (n <= g_break) {
+        double sum, **p = a, **q = b, **r = c;
         int i, j, k;
 
         for (i = 0; i < n; i++) {
@@ -97,44 +65,50 @@ void multiply(int n, matrix_s a, matrix_s b, matrix_s c, matrix_s d)
             }
         }
     } else {
+        double ***p, ***q, ***r, ***s;
         n /= 2;
-        sub(n, a12, a22, d11);
-        add(n, b21, b22, d12);
-        multiply(n, d11, d12, c11, d21);
-        sub(n, a21, a11, d11);
-        add(n, b11, b12, d12);
-        multiply(n, d11, d12, c22, d21);
-        add(n, a11, a12, d11);
-        multiply(n, d11, b22, c12, d12);
-        sub(n, c11, c12, c11);
-        sub(n, b21, b11, d11);
-        multiply(n, a22, d11, c21, d12);
-        add(n, c21, c11, c11);
-        sub(n, b12, b22, d11);
-        multiply(n, a11, d11, d12, d21);
-        add(n, d12, c12, c12);
-        add(n, d12, c22, c22);
-        add(n, a21, a22, d11);
-        multiply(n, d11, b11, d12, d21);
-        add(n, d12, c21, c21);
-        sub(n, c22, d12, c22);
-        add(n, a11, a22, d11);
-        add(n, b11, b22, d12);
-        multiply(n, d11, d12, d21, d22);
-        add(n, d21, c11, c11);
-        add(n, d21, c22, c22);
+        p = arr_qsplit(a, n);
+        q = arr_qsplit(b, n);
+        r = arr_qsplit(c, n);
+        s = arr_qsplit(d, n);
+
+        s_sub(n, a12, a22, d11);
+        s_add(n, b21, b22, d12);
+        s_mul(n, d11, d12, c11, d21);
+        s_sub(n, a21, a11, d11);
+        s_add(n, b11, b12, d12);
+        s_mul(n, d11, d12, c22, d21);
+        s_add(n, a11, a12, d11);
+        s_mul(n, d11, b22, c12, d12);
+        s_sub(n, c11, c12, c11);
+        s_sub(n, b21, b11, d11);
+        s_mul(n, a22, d11, c21, d12);
+        s_add(n, c21, c11, c11);
+        s_sub(n, b12, b22, d11);
+        s_mul(n, a11, d11, d12, d21);
+        s_add(n, d12, c12, c12);
+        s_add(n, d12, c22, c22);
+        s_add(n, a21, a22, d11);
+        s_mul(n, d11, b11, d12, d21);
+        s_add(n, d12, c21, c21);
+        s_sub(n, c22, d12, c22);
+        s_add(n, a11, a22, d11);
+        s_add(n, b11, b22, d12);
+        s_mul(n, d11, d12, d21, d22);
+        s_add(n, d21, c11, c11);
+        s_add(n, d21, c22, c22);
+
+        arr_qsplit_recall(p);
+        arr_qsplit_recall(q);
+        arr_qsplit_recall(r);
+        arr_qsplit_recall(s);
     }
 }
 
-/* c = a+b */
-void add(int n, matrix_s a, matrix_s b, matrix_s c)
+void s_add(int n, double** a, double** b, double** c)
 {
-    update_matrix_s(a, n);
-    update_matrix_s(b, n);
-    update_matrix_s(c, n);
-
-    if (n <= BREAK) {
-        double **p = a->d, **q = b->d, **r = c->d;
+    if (n <= g_break) {
+        double **p = a, **q = b, **r = c;
         int i, j;
 
         for (i = 0; i < n; i++) {
@@ -143,22 +117,27 @@ void add(int n, matrix_s a, matrix_s b, matrix_s c)
             }
         }
     } else {
+        double ***p, ***q, ***r;
         n /= 2;
-        add(n, a11, b11, c11);
-        add(n, a12, b12, c12);
-        add(n, a21, b21, c21);
-        add(n, a22, b22, c22);
+        p = arr_qsplit(a, n);
+        q = arr_qsplit(b, n);
+        r = arr_qsplit(c, n);
+
+        s_add(n, a11, b11, c11);
+        s_add(n, a12, b12, c12);
+        s_add(n, a21, b21, c21);
+        s_add(n, a22, b22, c22);
+
+        arr_qsplit_recall(p);
+        arr_qsplit_recall(q);
+        arr_qsplit_recall(r);
     }
 }
 
-/* c = a-b */
-void sub(int n, matrix_s a, matrix_s b, matrix_s c)
+void s_sub(int n, double** a, double** b, double** c)
 {
-    update_matrix_s(a, n);
-    update_matrix_s(b, n);
-    update_matrix_s(c, n);
-    if (n <= BREAK) {
-        double **p = a->d, **q = b->d, **r = c->d;
+    if (n <= g_break) {
+        double **p = a, **q = b, **r = c;
         int i, j;
 
         for (i = 0; i < n; i++) {
@@ -167,11 +146,19 @@ void sub(int n, matrix_s a, matrix_s b, matrix_s c)
             }
         }
     } else {
-//        double*** p;
+        double ***p, ***q, ***r;
         n /= 2;
-        sub(n, a11, b11, c11);
-        sub(n, a12, b12, c12);
-        sub(n, a21, b21, c21);
-        sub(n, a22, b22, c22);
+        p = arr_qsplit(a, n);
+        q = arr_qsplit(b, n);
+        r = arr_qsplit(c, n);
+
+        s_sub(n, a11, b11, c11);
+        s_sub(n, a12, b12, c12);
+        s_sub(n, a21, b21, c21);
+        s_sub(n, a22, b22, c22);
+
+        arr_qsplit_recall(p);
+        arr_qsplit_recall(q);
+        arr_qsplit_recall(r);
     }
 }
