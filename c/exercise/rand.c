446b4ff583c150ce7c64462f5d63ee30cec6d177
Danny Jiang <xinyi904314@yahoo.com.tw>
2016-01-19 14:46:00 +0800

    add rand exercise

---
+++ c/exercise/rand.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  rand.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2016年01月19日 14時43分50秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    printf("%s\n", "rand program");
+    return 0;
+}
